9fe8bb9d5e998f89f2d7b108982a847f519a94b6
baojintong <123488257@qq.com>
2017-10-23 09:07:51 +0800

    null

---
+++ SpringBootDemo/SpringBootDemo/src/main/java/cn/jt/demo/Student.java
@@ -0,0 +1,11 @@
+package cn.jt.demo;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Student {
+
+}
